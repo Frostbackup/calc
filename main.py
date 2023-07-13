a63c7f24b0cce022781f8085ac4b867053a0e6e6
Ruslan Morozov <9870042@gmail.com>
2023-07-13 20:46:53 +0300

    Добавил обработку ошибок на вводе

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+try:
+    a = int(input('введите первое число:'))
+    b = int(input('введите второе число:'))
+    c = a + b
+    print(c)
+except:
+    print('вы ввели не число')
 
-a = int(input('введите первое число:'))
-b = int(input('введите второе число:'))
-c = a + b
-print(c)
